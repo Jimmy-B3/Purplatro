cf7655e3683d3dbdd656c2319e75a649d0fbbeb4
Jimmy-B3 <lightphoenix846@gmail.com>
2025-11-17 20:15:47 -0500

    Files

---
+++ main.lua
@@ -0,0 +1,137 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Purplatro
+--- MOD_ID: purplatromalverk
+--- PREFIX: purplatro
+--- MOD_AUTHOR: [Jimmy B]
+--- MOD_DESCRIPTION: Ever wanted balatro to be just... purple?
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture({
+	  key = 'purple_joker',
+    set = 'Joker',
+	  path = 'purplejokers.png',
+    loc_txt = {
+        name = 'Purple Jokers'
+    }
+})
+AltTexture({
+	  key = 'purple_tarot',
+    set = 'Tarot',
+	  path = 'purplecons.png',
+    loc_txt = {
+        name = 'Purple Tarots'
+    }
+})
+AltTexture({
+	  key = 'purple_planet',
+    set = 'Planet',
+	  path = 'purplecons.png',
+    loc_txt = {
+        name = 'Purple Planets'
+    }
+})
+AltTexture({
+	  key = 'purple_spectral',
+    set = 'Spectral',
+	  path = 'purplecons.png',
+      soul = 'purplebacks.png',
+    loc_txt = {
+        name = 'Purple Spectrals'
+    }
+})
+AltTexture({
+    key = 'purple_enhance',
+    set = 'Enhanced',
+    path = 'purplebacks.png',
+    loc_txt = {
+        name = 'Purple Enhancements'
+    }
+})
+AltTexture({
+    key = 'purple_seal',
+    set = 'Seal',
+    path = 'purplebacks.png',
+    loc_txt = {
+        name = 'Purple Seals'
+    }
+})
+AltTexture({
+    key = 'purple_deck',
+    set = 'Back',
+    path = 'purplebacks.png',
+    loc_txt = {
+        name = 'Purple Decks'
+    }
+})
+AltTexture({
+    key = 'purple_voucher',
+    set = 'Voucher',
+    path = 'purplevouchers.png',
+    loc_txt = {
+        name = 'Purple Vouchers'
+    }
+})
+AltTexture({
+    key = 'purple_booster',
+    set = 'Booster',
+    path = 'purplepacks.png',
+    loc_txt = {
+        name = 'Purple Booster Packs'
+    }
+})
+AltTexture({
+    key = 'purple_tag',
+    set = 'Tag',
+    path = 'purpletags.png',
+    loc_txt = {
+        name = 'Purple Tags'
+    }
+})
+AltTexture({
+    key = 'purple_blind',
+    set = 'Blind',
+    path = 'purpleblinds.png',
+    frames = 21,
+    loc_txt = {
+        name = 'Purple Blinds'
+    }
+})
+AltTexture({
+    key = 'purple_stake',
+    set = 'Stake',
+    path = 'purplestakes.png',
+    loc_txt = {
+        name = 'Purple Stakes'
+    }
+})
+AltTexture({
+    key = 'purple_sticker',
+    set = 'Sticker',
+    path = 'purplestickers.png',
+    loc_txt = {
+        name = 'Purple Stickers'
+    }
+})
+TexturePack({
+    key = 'Purplatro',
+    textures = {
+        'purplatro_purple_joker',
+        'purplatro_purple_tarot',
+        'purplatro_purple_planet',
+        'purplatro_purple_spectral',
+        'purplatro_purple_enhance',
+        'purplatro_purple_seal',
+        'purplatro_purple_deck',
+        'purplatro_purple_voucher',
+        'purplatro_purple_booster',
+        'purplatro_purple_tag',
+        'purplatro_purple_blind',
+        'purplatro_purple_stake',
+        'purplatro_purple_sticker',
+    },
+    loc_txt = {
+        name = 'Purplatro',
+        text = {'Regular Balatro but its purple'}
+    }
+})
